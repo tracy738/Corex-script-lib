10dd0acd71bb40afc97594cc3473a13e8aab265b
tracy738 <tracyalgarne1@gmail.com>
2026-08-12 15:03:10 +0800

    Add authentication middleware for admin access

---
+++ db/middleware/auth.js
@@ -0,0 +1,8 @@
+function requireAuth(req, res, next) {
+  if (req.session && req.session.isAdmin) {
+    return next();
+  }
+  return res.redirect('/admin/login');
+}
+
+module.exports = { requireAuth };
